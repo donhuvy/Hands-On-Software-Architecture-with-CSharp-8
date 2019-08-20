cf82338072949335ec5732a5bcb0e0debbea9814
Do Nhu Vy <donhuvy@hotmail.com>
2019-08-20 09:31:14 +0700

    Polish

--- ch02/scalability-sample/Controllers/HomeController.cs
+++ ch02/scalability-sample/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using scalability_sample.Models;
+using System.Diagnostics;
 
 namespace scalability_sample.Controllers
 {
